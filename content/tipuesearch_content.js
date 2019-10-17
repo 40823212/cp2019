28cc5f4f6205b3cdf73e3f8e32dab18d02499e87
40823212 <40823212@gm.nuf.edu.tw>
2019-10-17 20:59:02 +0800

    initial add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \xa0 \n https://dartpad.dev/?id=ee4cb849f06bd337c955d559c80d4e7b \n \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': 'W4 \n \n', 'tags': '', 'url': 'USB.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \xa0 \n https://dartpad.dev/?id=ee4cb849f06bd337c955d559c80d4e7b \n \n \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': 'W4 \n \n', 'tags': '', 'url': 'USB.html'}]};
